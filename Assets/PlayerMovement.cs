b2d9e929467dddcb076c4a9ab5d61d9077728a2d
akuya06 <phuochuynh.03022006@gmail.com>
2025-02-08 09:17:26 +0700

    them Camera Follow va Flip

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private bool isGrounded;
+    private bool facingRight = true;
 
     void Start()
     {
@@ -21,14 +23,52 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("Speed", Mathf.Abs(movement.x));
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
+
+
+        FlipCharacter(movement.x);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+
+    void FlipCharacter(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight)
+        {
+            facingRight = true;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+        else if (horizontal < 0 && facingRight)
+        {
+            facingRight = false;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
     }
 }
